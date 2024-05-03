62993e736ce8dcb1804cd78ba25d7c325cb0b912
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-05-03 13:12:54 +0900

    feat: INITIAL SETUP

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+console.log("EXECUTED");
+
+import moment from "moment";
+
+
+const currentTime = moment().format("YY MM DD");
+console.log(currentTime);
+
+const person: string = "Martin";
+const count: number = 100;
